45a8c65644872fa40beafe5fca0ceacf7bc073d5
Megan Comeau <mlcomeau99@gmail.com>
2020-07-13 20:35:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  const cardArray = [];
+  for (let i=0; i < names.length; i++) {
+    cardArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return cardArray;
+}
+
+function countDown(num) {
+    while (num > 0) {
+      console.log(num);
+      num -= 1;
+    }
+    console.log(num);
+}
