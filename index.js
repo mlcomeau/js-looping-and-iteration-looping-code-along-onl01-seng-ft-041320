461bec4294a047983091d00bc0efd7285a484c91
Megan Comeau <mlcomeau99@gmail.com>
2020-07-13 20:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Code your solutions in this file
 
 function writeCards(names, eventName) {
+  let cardArray = []
   for (let i=0; names.length - 1, i++) {
-    let cardArray = []
+    cardArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
   }
+
+  return cardArray
 }
